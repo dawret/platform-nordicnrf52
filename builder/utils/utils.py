40ce1418d4e1c21476fb61fa07b4305104ae7982
dawret <dawret@dawret.me>
2026-01-24 11:56:01 +0100

    [CI] linux-aarch64 support

--- builder/utils/utils.py
+++ builder/utils/utils.py
@@ -16,6 +16,17 @@ def exec_command(cmd, msg, **kwargs):
     return result
 
 
+def is_arm64():
+    machine = platform.machine().lower()
+    return machine in ("aarch64", "arm64")
+
+
+def is_amd64():
+    machine = platform.machine().lower()
+    return machine in ("x86_64", "amd64")
+
+
+# For downloading Zephyr SDK
 def get_platform_string():
     os_map = {
         "linux": "linux",
@@ -23,40 +34,35 @@ def get_platform_string():
         "windows": "windows",
     }
 
-    arch_map = {
-        "x86_64": "x86_64",
-        "amd64": "x86_64",
-        "aarch64": "aarch64",
-        "arm64": "aarch64",
-    }
     system = platform.system().lower()
     machine = platform.machine().lower()
 
     os_name = os_map.get(system, system)
-    arch_name = arch_map.get(machine, machine)
+    arch_name = "aarch64" if is_arm64() else "x86_64" if is_amd64() else machine
 
     return f"{os_name}-{arch_name}"
 
 
+# For downloading nrfutil
 def get_platform_slug():
     if platform.system().lower() == "windows":
         print(f"Comparison: {platform.machine() != 'AMD64'}")
-        if platform.machine() != "x86_64" and platform.machine() != "AMD64":
+        if not is_amd64():
             raise RuntimeError(f"Unsupported architecture: {platform.machine()} on Windows")
         return "x86_64-pc-windows-msvc"
     elif platform.system().lower() == "linux":
-        if platform.machine() == "x86_64":
+        if is_amd64():
             return "x86_64-unknown-linux-gnu"
-        elif platform.machine() == "arm64" or platform.machine() == "aarch64":
+        elif is_arm64():
             return "aarch64-unknown-linux-gnu"
         else:
             raise RuntimeError(
                 f"Unsupported architecture: {platform.machine()} on Linux",
             )
     elif platform.system().lower() == "darwin":
-        if platform.machine() == "x86_64":
+        if is_amd64():
             return "x86_64-apple-darwin"
-        elif platform.machine() == "arm64":
+        elif is_arm64():
             return "aarch64-apple-darwin"
         else:
             raise RuntimeError(
