d5db81c86e22df6d432f13b9f78085523e949035
dawret <dawret@dawret.me>
2026-01-25 23:16:51 +0100

    Make sure another NCS installation isn't picked up

--- scripts/build_samples.py
+++ scripts/build_samples.py
@@ -5,6 +5,21 @@ import json
 import configparser
 import click
 
+ROOT_DIR = Path(__file__).parent.parent.resolve()
+
+SAMPLES = [
+    "zephyr/samples/basic/blinky",
+    "nrf/samples/openthread/cli",
+    "nrf/samples/bluetooth/peripheral_uart",
+]
+
+BOARDS = {
+    "xiao_ble": "xiao_ble",
+    "adafruit_feather_nrf52840": "adafruit_feather_nrf52840",
+    "nrf5340dk": "nrf5340dk/nrf5340/cpuapp",
+    "nrf54l15dk": "nrf54l15dk/nrf54l15/cpuapp",
+}
+
 
 def run_pio(args):
     result = subprocess.run(["pio"] + args, capture_output=True, text=True)
@@ -13,8 +28,8 @@ def run_pio(args):
     return result.stdout.strip()
 
 
-def get_env(samples_dir: Path, ini: Path):
-    stdout = run_pio(["run", "-d", str(samples_dir), "-c", str(ini.absolute()), "--target", "dump_env"])
+def get_env(build_dir: Path, ini: Path):
+    stdout = run_pio(["run", "-d", str(build_dir), "-c", str(ini.absolute()), "--target", "dump_env"])
     if match := re.search(r"==== Build Environment ====\n(.*?)\n==== End Build Environment ====", stdout, re.DOTALL):
         env_str = match.group(1)
         return json.loads(env_str)
@@ -57,12 +72,28 @@ def generate_sample_platformio_ini(build_dir: Path, sample_name: str, board: str
     return build_dir / f"platformio_{sample_name}.ini"
 
 
-def build_sample(sample, sdk_dir, build_dir, board, sdk_version):
+def generate_board_file(boards_dir: Path, board_id: str, board_name: str):
+    board_file = boards_dir / f"{board_id}.json"
+    board_data = {
+        "build": {"bsp": {"name": "adafruit"}, "zephyr": {"variant": board_name}, "softdevice": {"sd_fwid": "0x00B6"}},
+        "url": "https://esphome.io/",
+        "vendor": "esphome",
+        "frameworks": ["zephyr"],
+        "name": board_id,
+        "upload": {"maximum_ram_size": 248832, "maximum_size": 815104, "speed": 115200},
+    }
+    boards_dir.mkdir(parents=True, exist_ok=True)
+    with open(board_file, "w") as f:
+        json.dump(board_data, f, indent=4)
+    return board_file
+
+
+def build_sample(sample, sdk_dir, build_dir, board, boards_dir, sdk_version):
     sample_dir = sdk_dir / sample
     sample_name = sample_dir.name
     samples_dir = sample_dir.parent
-    ini = generate_sample_platformio_ini(build_dir, sample_name, board, Path("samples/boards"), sdk_version)
-    print(f"Building sample '{sample_name}'...")
+    ini = generate_sample_platformio_ini(build_dir, sample_name, board, boards_dir, sdk_version)
+    print(f"Building sample '{sample}' for board: {board}...")
     run_pio(["run", "-d", str(samples_dir), "-c", str(ini.absolute())])
     if not (build_dir / sample_name / "sample_env" / "merged.hex").is_file():
         raise RuntimeError(f"Failed to build sample '{sample_name}'")
@@ -70,19 +101,20 @@ def build_sample(sample, sdk_dir, build_dir, board, sdk_version):
 
 @click.command()
 @click.option("-d", "--build-dir", type=str, default=Path("build"))
-@click.option("-b", "--board", type=str)
 @click.option("-sv", "--sdk_version", type=str, default="v2.9.2")
-@click.argument("sample", type=str, nargs=-1)
-def main(build_dir, board, sdk_version, sample):
+def main(build_dir, sdk_version):
     build_dir = Path(build_dir)
     build_dir.mkdir(parents=True, exist_ok=True)
-    ini = generate_env_platformio_ini(build_dir, sdk_version, board)
-    env = get_env(Path("samples"), ini)
-    sdk_dir = Path(env["NRF_SDK_DIR"])
-    with open(build_dir / "dummy.c", "w") as f:
-        f.write("int main() { return 0; }")
-    for s in sample:
-        build_sample(s, sdk_dir, build_dir, board, sdk_version)
+    for b_id, b_name in BOARDS.items():
+        board_dir = build_dir / "boards"
+        generate_board_file(board_dir, b_id, b_name)
+        ini = generate_env_platformio_ini(build_dir, sdk_version, b_id)
+        env = get_env(build_dir, ini)
+        sdk_dir = Path(env["NRF_SDK_DIR"])
+        with open(build_dir / "dummy.c", "w") as f:
+            f.write("int main() { return 0; }")
+        for s in SAMPLES:
+            build_sample(s, sdk_dir, build_dir, b_id, board_dir, sdk_version)
 
 
 if __name__ == "__main__":
